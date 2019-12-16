3e7088b4d0979a371e92c72df0d381fccc2b24ca
rhdez11 <49217419+rhdez11@users.noreply.github.com>
2019-12-15 22:37:07 -0600

    Update camelCaseMethod.java

--- CamelCase Method/camelCaseMethod.java
+++ CamelCase Method/camelCaseMethod.java
@@ -7,21 +7,21 @@ public class Solution {
         
         StringBuilder str = new StringBuilder(string);
 		
-    		str.setCharAt(0, Character.toLowerCase(str.charAt(0)));
-    		int l= 0;
-    		
-    		for (int i=0; i<str.length();i++){
-    		    if(str.charAt(i)==' '){
-    		        l=str.length()-1;
-    		        str.deleteCharAt(i);
-    		        if(i!= l){
-    		            str.setCharAt(i, Character.toUpperCase(str.charAt(i)));
-        		        if(str.charAt(i)==' '){
-        		            i--;
-        		        }
-    		        }
-    		    }
-    		} 
+	str.setCharAt(0, Character.toLowerCase(str.charAt(0)));
+	int l= 0;
+
+	for (int i=0; i<str.length();i++){
+	    if(str.charAt(i)==' '){
+		l=str.length()-1;
+		str.deleteCharAt(i);
+		if(i!= l){
+		    str.setCharAt(i, Character.toUpperCase(str.charAt(i)));
+			if(str.charAt(i)==' '){
+			    i--;
+			}
+		}
+	    }
+	} 
         return str.toString();
     }
 }
